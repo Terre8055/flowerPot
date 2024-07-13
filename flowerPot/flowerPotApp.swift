dc9e9d81a4b2f0b53d44dfce62f968c90fa1a06c
Michael Appiah Dankwah <michaelappiah2018@icloud.com>
2024-07-13 11:34:56 +0000

    Initial Commit

---
+++ flowerPot/flowerPotApp.swift
@@ -0,0 +1,17 @@
+//
+//  flowerPotApp.swift
+//  flowerPot
+//
+//  Created by Mike on 13/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct flowerPotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
